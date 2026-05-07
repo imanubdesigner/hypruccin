f8fd6a9ef4c50974fda1586a46e23573138e8331
Manuel B. <imanubdesigner@gmail.com>
2026-05-07 13:24:46 +0200

    Fix applications

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -12,12 +12,20 @@ print_info "\nStarting utilities setup..."
 # -------------------- Utilities (AUR) --------------------
 run_command "yay -S --sudoloop --noconfirm --needed helium-browser-bin fastmod" "Install Utilities" "no" "no"
 
-# -------------------- Applications & mimeinfo --------------------
+# -------------------- Applications & icons --------------------
 run_command "\
 mkdir -p /home/$SUDO_USER/.local/share/applications && \
-cp -r $BASE_DIR/assets/manu/applications/* /home/$SUDO_USER/.local/share/applications/ && \
-chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.local/share/applications" \
-  "Copy all .desktop and mime files from assets/applications to local share" "yes" "no"
+mkdir -p /home/$SUDO_USER/.local/share/applications/icons && \
+mkdir -p /home/$SUDO_USER/.local/share/icons/hicolor/48x48/apps && \
+cp $BASE_DIR/assets/manu/applications/*.desktop /home/$SUDO_USER/.local/share/applications/ && \
+cp $BASE_DIR/assets/manu/applications/hidden/*.desktop /home/$SUDO_USER/.local/share/applications/ && \
+cp $BASE_DIR/assets/manu/applications/icons/*.png /home/$SUDO_USER/.local/share/applications/icons/ && \
+cp $BASE_DIR/assets/manu/applications/icons/*.png /home/$SUDO_USER/.local/share/icons/hicolor/48x48/apps/ && \
+chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.local/share/applications && \
+chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.local/share/icons/hicolor/48x48/apps && \
+gtk-update-icon-cache -f /home/$SUDO_USER/.local/share/icons/hicolor/ 2>/dev/null || true && \
+update-desktop-database /home/$SUDO_USER/.local/share/applications/ 2>/dev/null || true" \
+  "Install applications, icons and update caches" "yes" "no"
 
 # -------------------- .XCompose --------------------
 run_command "cp $BASE_DIR/assets/.XCompose /home/$SUDO_USER/ && chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.XCompose" "Copy .XCompose configuration to home directory" "no" "no"
